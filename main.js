5b49cc322a4d28d1cce2a41ca62648a24e95d375
Souradip Ghosh <souradipghosh882@gmail.com>
2020-07-02 17:25:38 +0530

    my personal portfolio

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     });
     $('nav a[href*="#"]').on('click',function(){
         $('html,body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
+            scrollTop: $($(this).attr('href')).offset().top -100
         },     2000);
     });
     $('#up').on('click',function(){
